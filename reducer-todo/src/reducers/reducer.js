9ceb15cd4c9b765e198a2851547832fe730ca225
Psage23 <pamelasage3@gmail.com>
2019-12-10 22:32:51 -0700

    MVP

--- reducer-todo/src/reducers/reducer.js
+++ reducer-todo/src/reducers/reducer.js
@@ -3,15 +3,43 @@ export const initialTodosState = [
         item: 'Shop',
         completed: true,
         id: 1
+    },
+    {
+        item: 'Wrap presents',
+        completed: false,
+        id: 2
+    },
+    {
+        item: 'Christmas party',
+        completed: false,
+        id: 3
     }
 ];
 
 export const todoReducer = (state, action) => {
-    switch(action, type) {
+    switch(action.type) {
         case 'ADD_TODO':
-            return [...state]
+            return [...state, 
+            {
+                item: action.payload,
+                completed: false,
+                id: Date.now()
+            }]
 
-            default:
-                return state;
+        case('TOGGLE_COMPLETED'):
+            return (
+                state.map((todo) => {
+                    if(todo.id === action.payload) {
+                        return {...todo, completed: !todo.completed}
+                    }
+                    return todo;
+                })
+            )
+
+        case('CLEAR_COMPLETED'):
+            return (
+                state.filter(todo => !todo.completed)
+            )
+            default: return state;
     }
 }
